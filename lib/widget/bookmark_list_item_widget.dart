bd5f62718d46ca58b69867eca2e01ebe3a75d6e1
ayilmaz4646 <ahmet.yilmaz@nimbo.com.tr>
2020-08-08 22:40:40 +0300

    list widget changed

--- lib/widget/bookmark_list_item_widget.dart
+++ lib/widget/bookmark_list_item_widget.dart
@@ -1,4 +1,3 @@
-import 'package:caster/page/view_bookmark_page.dart';
 import 'package:caster/util/navigation_util.dart';
 import "package:flutter/material.dart";
 import 'package:caster/model/bookmark.dart';
@@ -16,16 +15,14 @@ class BookmarkListItemWidget extends StatelessWidget {
       child: InkWell(
         onTap: () => navigateToViewBookmarkPage(bookmark, context),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // ignore: deprecated_member_use
-            Text(bookmark.title, style: Theme.of(context).textTheme.title),
-            SizedBox(height: 6),
-            // ignore: deprecated_member_use
-            Text(bookmark.link, style: Theme.of(context).textTheme.subhead),
-          ]
-
-        ),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // ignore: deprecated_member_use
+              Text(bookmark.title, style: Theme.of(context).textTheme.title),
+              SizedBox(height: 6),
+              // ignore: deprecated_member_use
+              Text(bookmark.link, style: Theme.of(context).textTheme.subhead),
+            ]),
       ),
     );
   }
--- lib/widget/bookmarks_list_widget.dart
+++ lib/widget/bookmarks_list_widget.dart
@@ -3,24 +3,16 @@ import 'package:caster/model/bookmark.dart';
 import 'package:caster/widget/bookmark_list_item_widget.dart';
 
 class BookmarksListWidget extends StatelessWidget {
-  List<Bookmark> bookmarksList;
+  final List<Bookmark> bookmarksList;
 
   BookmarksListWidget(this.bookmarksList);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: getBookmarkWidgetsList(),
-    );
-  }
-
-  List<BookmarkListItemWidget> getBookmarkWidgetsList() {
-    List<BookmarkListItemWidget> list = [];
-
-    for (Bookmark bookmark in bookmarksList) {
-      list.add(BookmarkListItemWidget(bookmark));
-    }
-
-    return list;
+    return ListView.builder(
+        itemCount: bookmarksList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return BookmarkListItemWidget(bookmarksList[index]);
+        });
   }
 }
